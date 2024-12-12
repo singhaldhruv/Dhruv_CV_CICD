6edd837de78aebdd808c5565c292336f286036a5
singhaldhruv <ds.dhruvsinghal@gmail.com>
2024-12-12 16:43:24 +0530

    19th Commit

--- script-cloudfront-invalidation-automation.py
+++ script-cloudfront-invalidation-automation.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     # Example usage
 
     # Replace with your CloudFront distribution ID
-    DISTRIBUTION_ID = "E25ZYMOMSLSBM8"
+    DISTRIBUTION_ID = "E2AO23OOLGI43X"
 
     # Replace with the paths you want to invalidate
     PATHS_TO_INVALIDATE = [
